8c2aa2b1c437fae6e77510b998935c3ac5075a5f
shravan20 <mrshravankumarb@gmail.com>
2021-05-05 19:39:28 +0530

    [ Configuration for JobRunnr ] feat | Configuration for JobRunnr Setup

---
+++ src/main/java/com/nineleaps/cron/configurations/MainConfiguration.java
@@ -0,0 +1,22 @@
+package com.nineleaps.cron.configurations;
+
+import org.jobrunr.jobs.mappers.JobMapper;
+import org.jobrunr.scheduling.JobScheduler;
+import org.jobrunr.storage.InMemoryStorageProvider;
+import org.jobrunr.storage.StorageProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MainConfiguration {
+
+
+    @Bean
+    public StorageProvider storageProvider(JobMapper jobMapper) {
+        InMemoryStorageProvider storageProvider = new InMemoryStorageProvider();
+        storageProvider.setJobMapper(jobMapper);
+        return storageProvider;
+    }
+
+}
